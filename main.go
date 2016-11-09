08fad87ba6920067338580025993ced199b25e79
Robert Carlsen <rwcarlsen@gmail.com>
2016-11-09 11:24:11 -0700

    tmp

--- main.go
+++ main.go
@@ -5,32 +5,23 @@ import (
 	"io"
 	"os"
 	"log"
+	"sort"
 	
-	"github.com/gonum/mat/mat64"
+	"github.com/gonum/matrix/mat64"
+	"github.com/gonum/integrate/quad"
 )
 
 func main() {
-	pts := []Point{
-		{0, 2},
-		{1, 3},
-		{2, 9},
-		{3, 1},
-	}
-	weights := []float64{
-		1,
-		1,
-		1,
-		1,
-	}
+	xs := []float64{0, 1, 2, 3}
 
 	n := 100
 
-	elem := NewElement(pts, weights)
+	elem := NewElement(xs)
 	elem.PrintShapeFuncs(os.Stdout, n)
 	//elem.PrintFunc(os.Stdout, n)
 	
-	xs := []float64{0, 1, 2, 3, 4, 5, 6}
-	mesh, err := NewMesh(xs, 3)
+	xs = []float64{0, 1, 2, 3, 4, 5, 6}
+	mesh, err := NewMesh(xs, 3, Boundary{}, Boundary{})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -51,13 +42,13 @@ func main() {
 type KernelParams struct {
 	X float64
 	U float64
-	DU float64
+	GradU float64
 	W float64
-	DW float64
+	GradW float64
 }
 
-type Kernel Interface {
-	Residual(p *KernelParams) float64
+type Kerneler interface {
+	Kernel(p *KernelParams) float64
 }
 
 type NodeId int
@@ -67,14 +58,15 @@ type Mesh struct {
 	Nodes []*Node
 	NodeIds map[*Node]NodeId
 	Left, Right Boundary
-	nextId int
+	next NodeId
 }
 
 // aka essential boundary condition matrix
-func (m *Mesh) essentialBC() *mat.Mat64 {
+func (m *Mesh) essentialBC() *mat64.Dense {
 	for _, e := range m.Elems {
 		
 	}
+	panic("unimplemented")
 }
 
 func (m *Mesh) Interpolate(x float64) float64 {
@@ -88,9 +80,9 @@ func (m *Mesh) Interpolate(x float64) float64 {
 
 func (m *Mesh) addNodes(ns ...*Node) {
 	for _, n := range ns {
-		m.NodeIds[n] = m.nextId
+		m.NodeIds[n] = m.next
 		m.Nodes = append(m.Nodes, n)
-		m.nextId++
+		m.next++
 	}
 }
 
@@ -101,7 +93,7 @@ const (
 	Dirichlet
 )
 
-type Boundary {
+type Boundary struct {
 	Val float64
 	Type BoundaryType
 }
@@ -113,36 +105,26 @@ func NewMesh(nodePos []float64, degree int, left, right Boundary) (*Mesh, error)
 		return nil, fmt.Errorf("incompatible mesh degree (%v) and node count (%v)", degree, len(nodePos))
 	}
 	
-	for i := range nodePos {
-	}
-	
 	nElems := (len(nodePos)-1) / (degree-1)
 	for i := 0; i < nElems; i++ {
-		pts := make([]Point, degree)
-		weights := make([]float64, degree)
+		xs := make([]float64, degree)
 		for j := 0; j < degree; j++ {
-			pts[j] = Point{nodePos[i*(degree-1)+j], 1}
-			weights[j] = 1
+			xs[j] = nodePos[i*(degree-1)+j]
 		}
-		elem := NewElement(pts, weights)
+		elem := NewElement(xs)
 		m.Elems = append(m.Elems, elem)
 		
-		if i == 0 {
-			m.addNodes(elem.Nodes[0])
+		if i > 0 {
+			elem.Connect(m.Elems[i-1])
+			m.addNodes(elem.Nodes...)
 		}
 		m.addNodes(elem.Nodes[1:]...)
-		
-		// have adjacent elements share edge node
-		if i > 0 {
-			prevNodes := m.Elems[i-1].Nodes
-			elem.Nodes[0] = prevNodes[len(prevNodes)-1]
-		}
 	}
 	return m, nil
 }
 
 // stiffness matrix
-func (m *Mesh) BuildSystem(k Kernel) (A, b mat64.Dense) {
+func (m *Mesh) BuildSystem(k Kerneler) (A, b mat64.Dense) {
 	for i, e := range m.Elems {
 		for j, n := range e.Nodes {
 			fn := func(x float64) float64 {
@@ -153,11 +135,12 @@ func (m *Mesh) BuildSystem(k Kernel) (A, b mat64.Dense) {
 					W: n.SampleWeight(x),
 					GradW: n.DerivWeight(x),
 				}
-				return k.Residual(pars)
+				return k.Kernel(pars)
 			}
 			residual := quad.Fixed(fn, e.Left(), e.Right(), len(e.Nodes), quad.Legendre{}, 0)
 		}
 	}
+	panic("unimplemented")
 }
 
 type Point struct {
@@ -173,7 +156,7 @@ type Point struct {
 //    (x1 - x2)   (x1 - x3)   (x1 - x4)
 //
 type Node struct {
-	Xmain float64
+	Index int
 	LeftZeros []float64
 	// RightZeros is only non-nil if the node is "shared" between two elements
 	RightZeros []float64
@@ -181,47 +164,62 @@ type Node struct {
 	Weight float64
 }
 
-func NewNode(p Point, leftZeros, rightZeros []float64, weight float64) *Node {
+func NewNode(xIndex int, xs ...float64) *Node {
+	zeros := append([]float64{}, xs...)
 	return &Node{
-		Xmain: p.X,
-		LeftZeros: leftZeros,
-		RightZeros: rightZeros,
-		Val:   p.Y,
-		Weight: weight,
+		Index: xIndex,
+		LeftZeros: zeros,
+		Val:   1,
+		Weight: 1,
 	}
 }
 
-func (n *Node) Update(val, weight float64) {
-	n.Val = val
-	n.Weight = weight
-}
-
-func (n *Node) zeros(x float64) []float64 {
+// x is ignored for interior nodes
+func (n *Node) Xvals(x float64) []float64 {
 	if len(n.LeftZeros) == 0 {
 		return n.RightZeros
 	} else if len(n.RightZeros) == 0 {
 		return n.LeftZeros
-	} else if x <= n.Xmain {
+	} else if x[0] <= n.Xmain {
 		return n.LeftZeros
 	}
 	return n.RightZeros
 }
 
+func NewNodeShared(xsLeft, xsRight []float64) *Node {
+	n := NewNode(len(xsLeft)-1, xsLeft)
+	tmp := NewNode(len(xsRight)-1, xsRight)
+	n.RightZeros = tmp.LeftZeros
+	return n
+}
+
+func (n *Node) ShareLeft(xsLeft []float64) {
+	if n.Xmain != n.LeftZeros[0] {
+		panic("interior/right node cannot be shared with left set of zeros")
+	} else if n.Xmain != xsLeft[len(xsLeft)-1] {
+		panic("left zeros do not share point with node's left edge")
+	}
+	*n = *NewNodeShared(xsLeft, n.Xvals())
+}
+
+func (n *Node) Xmain() float64 {return n.LeftZeros[n.Index]}
+
 // Deriv returns the derivative of the shape function at x.
 func (n *Node) Deriv(x float64) float64 {
-	zeros := n.zeros(x)
+	zeros := n.Xvals(x)
 	u := n.Val
 	dudx := 0.0
 	for _, x0 := range n.Xzero {
-		dudx = 1 / (n.Xmain - x0) * u + (x - x0) / (n.Xmain - x0) * dudx
-		u *= (x - x0) / (n.Xmain - x0)
+		if 
+		dudx = 1 / (n.Xmain() - x0) * u + (x - x0) / (n.Xmain() - x0) * dudx
+		u *= (x - x0) / (n.Xmain() - x0)
 	}
 	return dudx
 }
 
 // Sample returns the value of the shape function at x.
 func (n *Node) Sample(x float64) float64 {
-	zeros := n.zeros(x)
+	zeros := n.Xvals(x)
 	u := n.Val
 	for _, x0 := range zeros {
 		u *= (x - x0) / (n.Xmain - x0)
@@ -256,22 +254,27 @@ func (e *Element) Right() float64 {return e.NodePos[len(e.NodePos)-1]}
 func (e *Element) LeftNode() float64 {return e.Nodes[0]}
 func (e *Element) RightNode() float64 {return e.Nodes[len(e.NodePos)-1]}
 
-func NewElement(pts []Point, weights []float64, left, right *Element) *Element {
-	e := &Element{}
-	for _, p := range pts {
-		e.NodePos = append(e.NodePos, p.X)
-	}
-
-	for i, p := range pts {
-		leftzeros := append([]float64{}, e.NodePos[:i]...)
-		leftzeros = append(leftzeros, e.NodePos[i+1:]...)
-		var rightzeros []float64
-		if left != nil && i == 0 {
-			rightzeros = left.LeftNode().
-		}
-		e.Nodes = append(e.Nodes, NewNode(p, zeros, nil, weights[i]))
+func NewElement(xs []float64) *Element {
+	e := &Element{NodePos: append([]float64{}, xs...)}
+	for i := range xs {
+		n := NewNode(i, xs)
+		e.Nodes = append(e.Nodes, n)
 	}
 	return e
+}
+
+func (e *Element) Connect(elem *Element) error {
+	if e.Left() != elem.Right() && e.Right() != elem.Left() {
+		return fmt.Errorf("elements do not share a node/edge")
+	}
+	
+	left, right := e, elem
+	if elem.Right() == e.Left() {
+		left, right = right, left
+	}
+	right.LeftNode().ShareLeft(left.RightNode().Xvals())
+	left.Nodes[len(left.Nodes)-1] = right.LeftNode()
+	return nil
 }
 
 func (e *Element) Interpolate(x float64) float64 {
