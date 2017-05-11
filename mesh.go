717f709d7e94c0fcb184c929bf5a6868f2332bf9
Robert Carlsen <rwcarlsen@gmail.com>
2017-05-11 11:42:37 -0600

    better iterative solver

--- mesh.go
+++ mesh.go
@@ -160,33 +160,65 @@ func (m *Mesh) Solve(k Kernel) error {
 	return nil
 }
 
-func (m *Mesh) SolveIter(k Kernel, maxiter int, tol float64) (iter int, err error) {
-	m.reset()
-	b := m.ForceMatrix(k)
+type GaussSeidel struct {
+	MaxIter int
+	Tol     float64
+}
 
-	prev := mat64.NewVector(m.NumDOF(), nil)
-	soln := mat64.NewVector(m.NumDOF(), nil)
+func (g *GaussSeidel) solveRow(i int, row, b, soln *mat64.Vector) {
+	acceleration := 1.8 // between 1.0 and 2.0
+	xold := soln.At(i, 0)
+	soln.SetVec(i, 0)
+	Ainverse := 1 / row.At(i, 0)
+	xnew := (1-acceleration)*xold +
+		acceleration*Ainverse*(b.At(i, 0)-mat64.Dot(row, soln))
+	soln.SetVec(i, xnew)
+}
+
+func (g *GaussSeidel) forwardIter(Ai func(row int) *mat64.Vector, b, soln *mat64.Vector) {
+	for i := 0; i < b.Len(); i++ {
+		g.solveRow(i, Ai(i), b, soln)
+	}
+}
+
+func (g *GaussSeidel) backwardIter(Ai func(row int) *mat64.Vector, b, soln *mat64.Vector) {
+	for i := b.Len() - 1; i >= 0; i-- {
+		g.solveRow(i, Ai(i), b, soln)
+	}
+}
+
+func (g *GaussSeidel) Solve(Ai func(row int) *mat64.Vector, b *mat64.Vector) (soln *mat64.Vector, niter int) {
+	prev := mat64.NewVector(b.Len(), nil)
+	soln = mat64.NewVector(b.Len(), nil)
+	soln.CloneVec(b)
 
 	n := 0
-	acceleration := 1.7 // between 1.0 and 2.0
-	for ; n < maxiter; n++ {
-		for i := 0; i < m.NumDOF(); i++ {
-			row := m.StiffnessRow(k, i)
-			xold := soln.At(i, 0)
-			soln.SetVec(i, 0)
-			xnew := (1-acceleration)*xold +
-				acceleration/row.At(i, 0)*(b.At(i, 0)-mat64.Dot(row, soln))
-			soln.SetVec(i, xnew)
-		}
+	for ; n < g.MaxIter; n++ {
+		g.forwardIter(Ai, b, soln)
+		g.backwardIter(Ai, b, soln)
 
+		// check convergence
 		var diff mat64.Vector
 		diff.SubVec(soln, prev)
-		if er := mat64.Norm(&diff, 2) / mat64.Norm(soln, 2); er < tol {
+		if er := mat64.Norm(&diff, 2) / mat64.Norm(soln, 2); er < g.Tol {
 			break
 		}
 		prev.CloneVec(soln)
 	}
+	return soln, n
+}
 
+// SolveIter uses a symmetric (forward-backward) Gauss-Seidel iterative algorithm with successive
+// over relaxation to solve the system.
+func (m *Mesh) SolveIter(k Kernel, maxiter int, tol float64) (iter int, err error) {
+	m.reset()
+	b := m.ForceMatrix(k)
+	Ai := func(row int) *mat64.Vector { return m.StiffnessRow(k, row) }
+
+	solver := &GaussSeidel{maxiter, tol}
+	soln, n := solver.Solve(Ai, b)
+
+	// update DOF/nodes with discovered solution
 	for i := 0; i < soln.Len(); i++ {
 		nodes := m.indexNode[i]
 		for _, n := range nodes {
