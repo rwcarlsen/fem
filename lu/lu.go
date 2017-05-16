ddafdb8ac8a602f489d92ac7fc857041901f66c5
Robert Carlsen <rwcarlsen@gmail.com>
2017-05-16 15:37:15 -0600

    sparse solver working so far

--- lu/lu.go
+++ lu/lu.go
@@ -32,6 +32,14 @@ const eps = 1e-10
 func (s *Sparse) NonzeroRows(col int) (row []int) { return s.nonzeroRow[col] }
 func (s *Sparse) NonzeroCols(row int) (col []int) { return s.nonzeroCol[row] }
 
+func (s *Sparse) Clone() *Sparse {
+	clone := NewSparse(s.size)
+	for k, v := range s.data {
+		clone.Set(k.row, k.col, v)
+	}
+	return clone
+}
+
 func (s *Sparse) T() mat64.Matrix     { return mat64.Transpose{s} }
 func (s *Sparse) Dims() (int, int)    { return s.size, s.size }
 func (s *Sparse) At(i, j int) float64 { return s.data[index{i, j}] }
@@ -40,20 +48,20 @@ func (s *Sparse) Set(i, j int, v float64) {
 		delete(s.data, index{i, j})
 		for ii, col := range s.nonzeroCol[i] {
 			if col == j {
-				s.nonzeroCol = append(s.nonzeroCol[:ii], s.nonzeroCol[ii+1:]...)
+				s.nonzeroCol[i] = append(s.nonzeroCol[i][:ii], s.nonzeroCol[i][ii+1:]...)
 				break
 			}
 		}
 		for ii, row := range s.nonzeroRow[j] {
 			if row == i {
-				s.nonzeroRow = append(s.nonzeroRow[:ii], s.nonzeroRow[ii+1:]...)
+				s.nonzeroRow[i] = append(s.nonzeroRow[i][:ii], s.nonzeroRow[i][ii+1:]...)
 				break
 			}
 		}
 		return
 	}
 
-	s.data[index{i, j}] = math.Abs(v)
+	s.data[index{i, j}] = v
 	s.nonzeroCol[i] = append(s.nonzeroCol[i], j)
 	s.nonzeroRow[j] = append(s.nonzeroRow[j], i)
 }
@@ -74,33 +82,42 @@ func RowMult(s *Sparse, row int, mult float64) {
 
 func GaussJordan(A *Sparse, b []float64) []float64 {
 	size, _ := A.Dims()
+	donerows := make(map[int]bool, size)
+	pivots := make([]int, size)
 	for j := 0; j < size; j++ {
 		// find a first row with a nonzero entry in column i on or below diagonal
 		// to use as the pivot row.
 		piv := -1
 		for i := 0; i < size; i++ {
-			if A.At(i, j) != 0 {
+			if A.At(i, j) != 0 && !donerows[i] {
 				piv = i
 				break
 			}
 		}
+		pivots[j] = piv
+		donerows[piv] = true
 
 		pval := A.At(piv, j)
 		bval := b[piv]
 		for _, i := range A.NonzeroRows(j) {
 			if i != piv {
-				mult := -pval / A.At(i, j)
+				mult := -A.At(i, j) / pval
 				RowCombination(A, piv, i, mult)
 				b[i] += bval * mult
 			}
 		}
 	}
-	for i := 0; i < size; i++ {
-		col := A.NonzeroCols(i)[0]
-		mult := 1 / A.At(i, col)
+
+	for j, i := range pivots {
+		mult := 1 / A.At(i, j)
 		RowMult(A, i, mult)
-		b[col] *= mult
+		b[i] *= mult
 	}
 
-	return b
+	x := make([]float64, size)
+	for i := range pivots {
+		x[i] = b[pivots[i]]
+	}
+
+	return x
 }
--- lu/lu_test.go
+++ lu/lu_test.go
@@ -1,7 +1,11 @@
 package lu
 
-import "testing"
-import "github.com/gonum/matrix/mat64"
+import (
+	"math"
+	"testing"
+
+	"github.com/gonum/matrix/mat64"
+)
 
 func TestGaussJordan(t *testing.T) {
 	var tests = []struct {
@@ -20,36 +24,74 @@ func TestGaussJordan(t *testing.T) {
 			wantx: []float64{1, 1, 1, 1},
 		}, {
 			vals: []float64{
-				0, 1, 0, 0,
-				2, 0, 0, 0,
+				0, 2, 0, 0,
+				1, 0, 0, 0,
 				0, 0, 3, 0,
 				0, 0, 0, 4,
 			},
 			b:     []float64{1, 2, 3, 4},
 			wantx: []float64{0.5, 2, 1, 1},
+		}, {
+			vals: []float64{
+				1, 1, 0, 0,
+				0, 2, 0, 0,
+				0, 0, 3, 0,
+				0, 0, 0, 4,
+			},
+			b:     []float64{1, 2, 3, 4},
+			wantx: []float64{0, 1, 1, 1},
+		}, {
+			vals: []float64{
+				.5, .5, 0, 0,
+				0, 2, 0, 0,
+				0, 0, 3, 0,
+				0, 0, 0, 4,
+			},
+			b:     []float64{1, 2, 3, 4},
+			wantx: []float64{1, 1, 1, 1},
+		}, {
+			vals: []float64{
+				0, 2, 0, 0,
+				.5, .5, 0, 0,
+				0, 0, 3, 0,
+				0, 0, 0, 4,
+			},
+			b:     []float64{1, 2, 3, 4},
+			wantx: []float64{3.5, .5, 1, 1},
 		},
 	}
 
 	for i, test := range tests {
 		size := len(test.b)
+
 		A := NewSparse(size)
 		for i := 0; i < size; i++ {
 			for j := 0; j < size; j++ {
 				A.Set(i, j, test.vals[i*size+j])
 			}
 		}
+		refA := A.Clone()
+		refb := make([]float64, len(test.b))
+		copy(refb, test.b)
 
-		t.Logf("test %v A=\n%v\nb=\n%v", i+1, mat64.Formatted(A), test.b)
+		var x mat64.Vector
+		x.SolveVec(mat64.NewDense(size, size, test.vals), mat64.NewVector(size, test.b))
+		want := x.RawVector().Data
 
 		gotx := GaussJordan(A, test.b)
 
-		for i := range test.wantx {
-			if gotx[i] != test.wantx[i] {
-				t.Errorf("    x: got %v, want %v", gotx, test.wantx)
+		failed := false
+		for i := range want {
+			if diff := math.Abs(gotx[i] - want[i]); diff > eps {
+				failed = true
+				break
 			}
 		}
-		if !t.Failed() {
-			t.Logf("PASSED")
+
+		if failed {
+			t.Errorf("test %v A=\n%v\nb=%v", i+1, mat64.Formatted(refA), refb)
+			t.Errorf("    x: got %v, want %v", gotx, want)
+			t.Errorf("    RREF:\n%.3v", mat64.Formatted(A))
 		}
 	}
 }
