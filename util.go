431bf333c351cf1a2c7f603f92179bc8c75fb972
Robert Carlsen <rwcarlsen@gmail.com>
2017-05-18 17:25:05 -0600

    remove unused types/funcs

--- util.go
+++ util.go
@@ -1,11 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"math"
-
-	"github.com/gonum/matrix/mat64"
-)
+import "math"
 
 func absInt(v int) int {
 	if v < 0 {
@@ -133,67 +128,4 @@ func permute(skip func([]int) bool, dimensions []int, prefix []int) [][]int {
 		set = append(set, permute(skip, dimensions[1:], newprefix)...)
 	}
 	return set
-}
-
-type Matrix interface {
-	mat64.Matrix
-	Set(i, j int, v float64)
-}
-
-// NBanded is a sparse matrix and implements the Matrix interface - which is a superset of the
-// mat64.Matrix interface and can be used in mat64 for matrix ops/solving.
-type NBanded struct {
-	size     int
-	nbands   int
-	diag     []float64
-	lowBands [][]float64
-	upBands  [][]float64
-}
-
-func NewNBanded(size, nbands int) *NBanded {
-	nb := &NBanded{
-		size:   size,
-		nbands: nbands,
-		diag:   make([]float64, size),
-	}
-	for i := 0; i < nbands; i++ {
-		nb.lowBands = append(nb.lowBands, make([]float64, size-i))
-		nb.upBands = append(nb.upBands, make([]float64, size-i))
-	}
-	return nb
-}
-
-func (nb *NBanded) BandWidth() (int, int) { return nb.nbands, nb.nbands }
-
-func (nb *NBanded) T() mat64.Matrix  { return mat64.Transpose{nb} }
-func (nb *NBanded) Dims() (int, int) { return nb.size, nb.size }
-
-func (nb *NBanded) Set(i, j int, v float64) {
-	b := i - j
-	if absInt(b) > nb.nbands { // off all bands
-		panic(fmt.Sprintf("index i,j (%v,%v) off all bands", i, j))
-	} else if b == 0 { // diag
-		nb.diag[i] = v
-	} else if b > 0 { // below diag
-		band := nb.lowBands[b-1]
-		band[j] = v
-	} else { // above diag
-		band := nb.upBands[-b-1]
-		band[i] = v
-	}
-}
-
-func (nb *NBanded) At(i, j int) float64 {
-	b := i - j
-	if absInt(b) > nb.nbands { // off all bands
-		return 0
-	} else if b == 0 { // diag
-		return nb.diag[i]
-	} else if b > 0 { // below diag
-		band := nb.lowBands[b-1]
-		return band[j]
-	} else { // above diag
-		band := nb.upBands[-b-1]
-		return band[i]
-	}
 }
